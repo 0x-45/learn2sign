1bac76b16c207a18cd95d6a97960469b3fdc57f5
Aniket Singh Rawat <sprx077@gmail.com>
2020-12-05 18:18:22 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout/Layout";
+import { AboutWrapper } from "../styles/AboutWrapper";
+
+export default function About() {
+  return (
+    <Layout>
+      <AboutWrapper>
+        <div className="heading">About</div>
+      </AboutWrapper>
+    </Layout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from "./../components/Layout/Layout";
+import { IndexWrapper } from "../styles/IndexWrapper";
+
+export default function Home() {
+  return (
+    <Layout>
+      <IndexWrapper>
+        <div className="heading">Home</div>
+      </IndexWrapper>
+    </Layout>
+  );
+}
